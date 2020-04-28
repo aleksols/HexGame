16af4c9251664e3dec6cbdc470778085e82e3364
Aleksander Scherman Olsen <aleko@stud.ntnu.no>
2020-04-28 19:06:32 +0200

    Remove unuseful comment

--- main.py
+++ main.py
@@ -116,7 +116,6 @@ def train():
     for i in range(1, len(losses)):
         avg.append((sum(avg[-9:]) + losses[i]) / (min(10, len(avg) + 1)))
     plt.plot(avg)
-    # print(dir(fig))
     plt.show()
     plt.plot(losses)
     plt.plot(avg)
